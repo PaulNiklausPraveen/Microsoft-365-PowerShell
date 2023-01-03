2b47aa5d01aa29a6153b0d9ee3ae52807cda88f4
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-03 08:41:42 +0000

    Update Connect-Exchange_Online.ps1

--- Connect-Exchange_Online.ps1
+++ Connect-Exchange_Online.ps1
@@ -1,14 +1,22 @@
 ﻿#This script will connect to Exchange online
 
 #Enter the credentials to connect to exchange online
-$UserCredential = Get-Credential
 
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
- -Credential $UserCredential -Authentication Basic -AllowRedirection
+$Username = "TYPE_USERPRINCIPALNAME_HERE"
+$planePassword = 'TYPE_PASSWORD_HERE'
 
-Import-PSSession $Session
+$password = ConvertTo-SecureString $planepassword -AsPlainText -Force
+$Cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $Username,$password
+
+#PSRemoting
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Cred -Authentication Basic -AllowRedirection
+#Import the session
+$import = import-pssession $Session -AllowClobber
+#Install MSONLINE Module
+Install-module msonline -Force | Import-Module
+
+Connect-MsolService -Credential $Cred
 
 
 #Test to confirm we are connected to Exchange online
 Get-Mailbox | select Name,Alias,UserPrincipalName,IsMailboxEnabled,Whenmailboxcreated   | FL
-
