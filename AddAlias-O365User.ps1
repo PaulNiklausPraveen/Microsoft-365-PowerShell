e94a930e753f0e35f1a4164b80a413e68efa1f5e
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-03 09:27:57 +0000

    Update AddAlias-O365User.ps1

--- AddAlias-O365User.ps1
+++ AddAlias-O365User.ps1
@@ -1,17 +1,12 @@
-﻿#This Script will install Msonline powershell module and add alias
+﻿#This Script will install Msonline powershell module and add alias to mail 
 
 #Installs Microsoft Azure Active Directory Module for Windows PowerShell
-Install-Module Msonline
-
-#Imports the module
-Import-Module Msonline
+Install-Module Msonline -Force | Import-Module 
 
 #Initiates a connection to Azure Active Directory
 connect-Msolservice
 
-
 Set-Mailbox -Identity  demouser@company.com -EmailAddresses @{Add="demouseralias@company.com"}   
-
 
 #Get the alias to verify the removal
 Get-Mailbox -Identity  demouser@company.com | Select -ExpandProperty emailaddresses alias
